810ea7e5778301523d7319a78175490f17d6f1f9
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-07-17 13:25:51 -0300

    update upload page

--- app/memories.tsx
+++ app/memories.tsx
@@ -1,8 +1,16 @@
-import { View, Text } from 'react-native'
-export default function Memories() {
+import { Link } from 'expo-router'
+import { View, Text, TouchableOpacity } from 'react-native'
+import Icon from '@expo/vector-icons/Feather'
+
+export default function NewMemory() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text>memories</Text>
+      <Link href="/new" asChild>
+        <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
+          <Icon name="arrow-left" size={16} color="#FFF" />
+        </TouchableOpacity>
+      </Link>
     </View>
   )
 }
